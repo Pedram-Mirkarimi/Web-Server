848cd611c71678e70c00eeba6dcde998ba415ed5
Seyed AmirMohammad Mirkarimi <mirkarimi.amirmohammad@gmail.com>
2026-01-05 14:48:12 +0330

    Enhance geocode function with token validation
    Added error handling for missing Mapbox token and improved response checks.

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,14 +1,25 @@
 const request = require('request');
 
 const geocode = (address, callback) => {
+  const token = process.env.MAPBOX_TOKEN;
+
+  if (!token) {
+    callback('Missing Mapbox token. Set MAPBOX_TOKEN in .env', undefined);
+    return;
+  }
+
   const url =
     'https://api.mapbox.com/geocoding/v5/mapbox.places/' +
     encodeURIComponent(address) +
-    '.json?access_token=pk.eyJ1IjoicGVkcmFtLW1pcmthcmltaSIsImEiOiJja2hycXFpZDMxeDViMnZteG5oNjY4dnczIn0.qOYMpxBWZYr57tX1PsQBLw&limit=1';
+    '.json?access_token=' +
+    token +
+    '&limit=1';
 
-  request({ url, json: true }, (error, { body }) => {
+  request({ url, json: true }, (error, { body } = {}) => {
     if (error) {
       callback('Unable to connect to location services!', undefined);
+    } else if (!body || !body.features) {
+      callback('Unable to get location data.', undefined);
     } else if (body.features.length === 0) {
       callback('Unable to find location. Try another search.', undefined);
     } else {
