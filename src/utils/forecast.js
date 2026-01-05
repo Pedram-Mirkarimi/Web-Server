430667c4e5220be2e5760cbb34f4e819d8b1fe74
Seyed AmirMohammad Mirkarimi <mirkarimi.amirmohammad@gmail.com>
2026-01-05 14:41:02 +0330

    Refactor forecast function to use API key from env
    Updated forecast function to use environment variable for API key and added error handling for missing data.

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,23 +1,34 @@
 const request = require('request');
 
 const forecast = (lat, lon, callback) => {
+  const apiKey = process.env.OPENWEATHER_KEY;
+
+  if (!apiKey) {
+    callback('Missing OpenWeather API key. Set OPENWEATHER_KEY in .env', undefined);
+    return;
+  }
+
   const url =
     'https://api.openweathermap.org/data/2.5/weather?lat=' +
     lat +
     '&lon=' +
     lon +
-    '&appid=0518b0f3bd1f28da019feb94c9e2f2af&units=metric';
+    '&appid=' +
+    apiKey +
+    '&units=metric';
 
-  request({ url, json: true }, (error, { body }) => {
+  request({ url, json: true }, (error, { body } = {}) => {
     if (error) {
       callback('Unable to connect the weather service!', undefined);
+    } else if (!body) {
+      callback('Unable to get weather data.', undefined);
     } else if (body.cod == 400) {
       callback('Unable to find location.', undefined);
     } else {
       callback(
         undefined,
         body.weather[0].description +
-          '. It\'s currently ' +
+          ". It's currently " +
           body.main.temp +
           ' degrees out.' +
           ' The humidity is ' +
